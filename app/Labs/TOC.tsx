035282133ba5ba0a5bd3b411bfe7fee82c103849
Ajin Frank Justin <ajinfrankj@gmail.com>
2025-09-29 00:01:39 -0400

    a2 CSS and Bootstrap

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
        <NavLink href="/" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/justin-aj/kambaz-next-js" target="_blank" id="wd-github">My GitHub Repository</NavLink>
+       <NavLink href="https://github.com/justin-aj/kambaz-next-js" target="_blank" id="wd-github">My GitHub</NavLink>
      </NavItem>
    </Nav>
 );}
